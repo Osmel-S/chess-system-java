a01c75798553be01fa60163796da02d18bdcd7cf
Tiosams <osmelzinho@gmail.com>
2020-11-03 22:22:43 -0300

    Chess  layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,22 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	
+	public Piece( Board board) {
+		this.board = board;
+		position = null; //(opcional,por padrao java ja joga valor nulo)
+	}
+	//posicao de uma piece recem criada vai ser nula
+	
+	protected Board getBoard() {
+		return board;
+	}
 
+
+
+	
 }
